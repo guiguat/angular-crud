4d5bb40bc35a6b3a2e5385822de161795830e78b
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-15 20:06:33 -0300

    create product object and create method

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { Router } from '@angular/router';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,13 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product:Product = {
+    name:'',
+    price: null
+  }
+
   constructor(private productService: ProductService, private router:Router) { }
 
   ngOnInit(): void {
   }
 
   createProduct():void{
-    this.productService.showMessage("Producto criado com sucesso!")
+    this.productService.create(this.product).subscribe(()=>{
+      this.productService.showMessage("Producto criado com sucesso!");
+      this.router.navigate(['/products']);
+    })
   }
   
   cancel():void{
